5e207fa8b9da35dc744fe2447c33dd712f119cc6
haydenlinder <haydenlinder@gmail.com>
2020-11-16 21:24:37 -0800

    Finish canvas-and-animation

--- src/App.js
+++ src/App.js
@@ -1,50 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import * as THREE from 'three';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { useRef } from 'react';
+
+const Box = () => {
+  const ref = useRef();
+  useFrame(state => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  });
+  
+  return(
+    <mesh ref={ref}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color='blue' />
+    </mesh>
+  )
+}
 
 function App() {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(
-    window.innerWidth,
-    window.innerHeight
-  );
-  document.body.innerHTML = '';
-  document.body.appendChild(renderer.domElement);
-
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({
-    color: 'blue'
-  });
-  camera.position.z = 5;
-  const cube = new THREE.Mesh(geometry,material);
-  scene.add(cube);
-
-  function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene,camera);
-  }
-
-  animate();
-
-  window.addEventListener('resize',
-    () => {
-      renderer.setSize(window.innerWidth, window.innerHeight)
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-    }
-  );
-
   return (
-    null
+    <div style={{height: '100vh', width: '100vw'}}>
+      <Canvas style={{background: 'black'}}>
+        <Box/>
+      </Canvas>
+    </div>
   );
 }
 
