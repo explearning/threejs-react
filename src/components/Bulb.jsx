b829bd57ef5b7abe2ddcc174237c9d82d7fef74e
haydenlinder <haydenlinder@gmail.com>
2020-12-04 21:50:21 -0800

    Finish model-shadow

--- src/components/Bulb.jsx
+++ src/components/Bulb.jsx
@@ -1,7 +1,12 @@
 const Bulb = props => {
     return (
         <mesh {...props}>
-            <pointLight castShadow />
+            <pointLight 
+                castShadow 
+                shadow-mapSize-height={2**10}
+                shadow-mapSize-width={2**10}
+                shadow-radius={10}
+            />
             <sphereBufferGeometry args={[0.2, 20, 20]} />
             <meshPhongMaterial emissive='yellow' />
         </mesh>
